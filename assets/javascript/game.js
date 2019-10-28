f7871bbf851e112ab1119e81fde17ef55dc4ea10
korends <koren.dlsantos@gmail.com>
2019-10-27 20:03:15 -0400

    finished js and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 
 // Computer choices
-var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Variables 
 var wins = 0;
 var loss = 0;
+var guessesLeft = 10;
 var guesses = [];
 
 // Call the html "p" tags
@@ -12,13 +13,43 @@ var guesses = [];
 var winsText = document.getElementById("wins-text");
 var lossText = document.getElementById("loss-text");
 var userguessText = document.getElementById("userguess-text");
+var guessesleftText = document.getElementById("guessesleft-text")
 
-// when user presses a key
+// Generate a random letter from the array
 
-document.addEventListener('keydown', function(e) {
-    const key = event.key.toLowerCase();
+function startGame() {
+    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+}
 
-    guesses.push(key);
-    console.log(guesses);
-});
+// when user presses a key to guess
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
+    if (userGuess === computerGuess) {
+        wins++;
+        guessesLeft = 10;
+        guesses = [];
+        startGame()
+    } if (userGuess !== computerGuess) {
+        guessesLeft--;
+        guesses.push(userGuess);
+    } if (guessesLeft === 0) {
+        loss++;
+        guessesLeft = 10;
+        guesses = [];
+        startGame();
+    }
+
+    winsText.textContent = "Wins: " + wins;
+    lossText.textContent = "Losses: " + loss;
+    guessesleftText.textContent = "Guesses left: " + guessesLeft;
+    userguessText.textContent = "Your guesses so far: ";
+    var newguesses = guesses.join(", ");
+
+    // guesses.push(userGuess);
+    document.getElementById("allguesses-text").innerHTML = newguesses;
+};
+
+startGame()
 
